e8548f4d899d5db6ff066f0c7bf2c38444457744
Ethan Granato <ethang@vertechgroup.com>
2018-08-30 19:42:08 -0600

    updated cors settings

--- app.js
+++ app.js
@@ -11,17 +11,14 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // CORS
 app.use((req, res, next) => {
-    const origin = req.headers['origin'];
-    if (origin === "http://www.projectmpenzi.com" || origin === "http://projectmpenzi.com") {
-        res.header('Access-Control-Allow-Origin', origin);
-    } else {
-        res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-    }
+    res.header('Access-Control-Allow-Origin', 'https://www.projectmpenzi.com');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Credentials', 'true');
